34a150baa5cdadc42b9c3e5aa0ae86481336d9f6
Hai Lam <lamthanhhai141@gmail.com>
2019-07-12 15:13:12 +0700

    first

---
+++ client2.js
@@ -0,0 +1,88 @@
+var config = {
+    sdpSemantics: 'unified-plan',
+    iceServers: [
+        {
+            urls: ['stun:stun.l.google.com:19302']
+        },
+        {
+            urls: ["turn:35.247.173.254:3478"],
+            username: "username",
+            credential: "password"
+        },
+        {
+            urls: ["turn:numb.viagenie.ca"],
+            credential: "muazkh",
+            username: "webrtc@live.com"
+        }
+    ]
+};
+
+
+var pc = new RTCPeerConnection(config)
+
+var iceConnectionLog = document.getElementById('ice-connection-state'),
+    iceGatheringLog = document.getElementById('ice-gathering-state'),
+    signalingLog = document.getElementById('signaling-state');
+
+let log = msg => {
+    document.getElementById('logs').innerHTML += msg + '<br>'
+}
+
+let displayVideo = video => {
+    var el = document.createElement('video')
+    el.srcObject = video
+    el.autoplay = true
+    el.muted = true
+    el.width = 160
+    el.height = 120
+
+    document.getElementById('localVideos').appendChild(el)
+    return video
+}
+
+function join() {    
+    let sd = document.getElementById('remoteSessionDescription').value
+    if (sd === '') {
+        return alert('Session Description must not be empty')
+    }
+    console.log(1)
+    // register some listeners to help debugging
+    pc.addEventListener('icegatheringstatechange', function() {
+        iceGatheringLog.textContent += ' -> ' + pc.iceGatheringState;
+    }, false);
+    iceGatheringLog.textContent = pc.iceGatheringState;
+
+    pc.addEventListener('iceconnectionstatechange', function() {
+        iceConnectionLog.textContent += ' -> ' + pc.iceConnectionState;
+    }, false);
+    iceConnectionLog.textContent = pc.iceConnectionState;
+
+    pc.addEventListener('signalingstatechange', function() {
+        signalingLog.textContent += ' -> ' + pc.signalingState;
+    }, false);
+    signalingLog.textContent = pc.signalingState;
+
+    // connect audio / video
+    pc.addEventListener('track', function (evt) {
+        console.log("Track event: ", evt)
+        if (evt.track.kind == 'video')
+          document.getElementById('video').srcObject = evt.streams[0];
+        else
+          document.getElementById('audio').srcObject = evt.streams[0];
+    });
+
+    console.log(2)
+    return pc.setRemoteDescription(new RTCSessionDescription(JSON.parse(atob(sd))))
+    .then(() => pc.createAnswer())
+    .then(answer => {
+        pc.setLocalDescription(answer)
+        console.log("here")
+        pc.oniceconnectionstatechange = e => log(pc.iceConnectionState)
+        pc.onicecandidate = event => {
+            if (event.candidate === null) {
+                document.getElementById('localSessionDescription').value = btoa(JSON.stringify(pc.localDescription))
+            }
+        }
+    })
+    .catch(e => alert(e))
+}
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.
+		HandleFunc("/client1", client1Handler)
+	router.
+		HandleFunc("/client2", client2Handler)
+	router.
+		HandleFunc("/client1.js", js1Handler)
+	router.
+		HandleFunc("/client2.js", js2Handler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	err := http.ListenAndServeTLS(fmt.Sprintf(":%s", port), "server.crt", "server.key", router)
+	// err := http.ListenAndServe(fmt.Sprintf(":%s", port), router)
+
+	if err != nil {
+		fmt.Print(err)
+	}
+}
+
+func client1Handler(w http.ResponseWriter, r *http.Request) {
+	path := fmt.Sprintf("client1.html")
+	fmt.Println(path)
+	http.ServeFile(w, r, path)
+}
+
+func client2Handler(w http.ResponseWriter, r *http.Request) {
+	path := fmt.Sprintf("client2.html")
+	fmt.Println(path)
+	http.ServeFile(w, r, path)
+}
+
+func js1Handler(w http.ResponseWriter, r *http.Request) {
+	path := fmt.Sprintf("client1.js")
+	fmt.Println(path)
+	http.ServeFile(w, r, path)
+}
+
+func js2Handler(w http.ResponseWriter, r *http.Request) {
+	path := fmt.Sprintf("client2.js")
+	fmt.Println(path)
+	http.ServeFile(w, r, path)
+}
